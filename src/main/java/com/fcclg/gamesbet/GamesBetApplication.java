33199abc5f10cf438689cf42e473664f6b00a873
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2024-11-12 09:31:39 +0100

    init project

---
+++ src/main/java/com/fcclg/gamesbet/GamesBetApplication.java
@@ -0,0 +1,13 @@
+package com.fcclg.gamesbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesBetApplication.class, args);
+    }
+
+}
